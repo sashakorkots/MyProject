753914c31abcc4dfa70565380aecfe6a71250b21
Olexander Korkots <k.sasha.g@gmail.com>
2021-05-11 17:27:44 +0300

    add class restaurant

---
+++ FoodDeliveryApi/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryApi.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string OpeningTime { get; set; }
+    }
+}
