bead940ebc97653f2b430054a1a5c0a5270f5835
Olexander Korkots <k.sasha.g@gmail.com>
2021-05-12 17:03:17 +0300

    create class dish

---
+++ FoodDeliveryApi/Models/Dish.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryApi.Models
+{
+    public class Dish
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int TypeId { get; set; }
+        public DishType Type { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public decimal Price { get; set; }
+    }
+}
