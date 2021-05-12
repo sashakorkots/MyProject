3914d4450662a393aaa8c282d646667768c6deef
Olexander Korkots <k.sasha.g@gmail.com>
2021-05-12 17:01:50 +0300

    create class dish type

---
+++ FoodDeliveryApi/Models/DishType.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodDeliveryApi.Models
+{
+    public class DishType
+    {
+        public int ID { get; set; }
+        public int Title { get; set; }
+    }
+}
