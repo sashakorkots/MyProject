040b621e0b50238cffe3558cbb6b0600315b54d4
Olexander Korkots <k.sasha.g@gmail.com>
2021-05-14 11:28:46 +0300

    Add migration for courier, order and orderDish

---
+++ FoodDeliveryApi/Migrations/20210514070853_CreateCourierAndOrderAndOrderDish.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FoodDeliveryApi.Migrations
+{
+    public partial class CreateCourierAndOrderAndOrderDish : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "couriers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    surname = table.Column<string>(type: "text", nullable: true),
+                    phone_number = table.Column<string>(type: "text", nullable: true),
+                    restaurant_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_couriers", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_couriers_restaurants_restaurant_id",
+                        column: x => x.restaurant_id,
+                        principalTable: "restaurants",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    delivery_address = table.Column<string>(type: "text", nullable: true),
+                    delivery_time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    total_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    courier_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_orders", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_orders_couriers_courier_id",
+                        column: x => x.courier_id,
+                        principalTable: "couriers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_dishes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    dish_id = table.Column<int>(type: "integer", nullable: false),
+                    order_id = table.Column<int>(type: "integer", nullable: false),
+                    count = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order_dishes", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_order_dishes_dishes_dish_id",
+                        column: x => x.dish_id,
+                        principalTable: "dishes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_order_dishes_orders_order_id",
+                        column: x => x.order_id,
+                        principalTable: "orders",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_couriers_restaurant_id",
+                table: "couriers",
+                column: "restaurant_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_order_dishes_dish_id",
+                table: "order_dishes",
+                column: "dish_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_order_dishes_order_id",
+                table: "order_dishes",
+                column: "order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_orders_courier_id",
+                table: "orders",
+                column: "courier_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_dishes");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "couriers");
+        }
+    }
+}
---
+++ FoodDeliveryApi/Migrations/20210514074948_AddRestaurantToOrder.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodDeliveryApi.Migrations
+{
+    public partial class AddRestaurantToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "restaurant_id",
+                table: "orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_orders_restaurant_id",
+                table: "orders",
+                column: "restaurant_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_orders_restaurants_restaurant_id",
+                table: "orders",
+                column: "restaurant_id",
+                principalTable: "restaurants",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_orders_restaurants_restaurant_id",
+                table: "orders");
+
+            migrationBuilder.DropIndex(
+                name: "ix_orders_restaurant_id",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "restaurant_id",
+                table: "orders");
+        }
+    }
+}
